330fadd1ab223e25716cec68fcedd192e2d8102c
Jefferson Richards <jefferson.richards@outlook.com>
2023-07-17 15:09:43 -0300

    Adicionado Requisição GET + Paginação e Ordenação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,11 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,22 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.service.MedicoService;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
-	
+
 	@Autowired
 	private MedicoService service;
-	
+
 	@PostMapping
 	@Transactional
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-		
-		service.insert(new Medico(dados));
-		
+
+		service.insert(dados);
+
+	}
+
+	@GetMapping
+	public Page<DadosListagemMedico> listar(@PageableDefault(size =10,sort = {"nome"}) Pageable paginacao) {
+			return service.findAll(paginacao);
 	}
 
 }
--- src/main/java/med/voll/api/service/MedicoService.java
+++ src/main/java/med/voll/api/service/MedicoService.java
@@ -1,8 +1,12 @@
 package med.voll.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.repositories.MedicoRepository;
 
@@ -13,8 +17,12 @@ public class MedicoService {
 	private MedicoRepository repository;
 
 	
-	public Medico insert(Medico obj) {
-		return repository.save(obj);
+	public Medico insert(DadosCadastroMedico dados) {
+		return repository.save(new Medico(dados));
+	}
+	
+	public Page<DadosListagemMedico> findAll(Pageable paginacao) {
+		return repository.findAll(paginacao).map(DadosListagemMedico::new);
 	}
 
 	
