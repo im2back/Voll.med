d49fcd415b4d436264ba7582c91025c313973812
Jefferson Richards <jefferson.richards@outlook.com>
2023-07-21 21:39:42 -0300

    Tratando erros genéricos e conflitos de duplicação de cadastro

--- src/main/java/med/voll/api/repositories/MedicoRepository.java
+++ src/main/java/med/voll/api/repositories/MedicoRepository.java
@@ -1,5 +1,7 @@
 package med.voll.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +12,11 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
 	Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 	
+	   List<Medico> findByCrm(String crm);
+	   
+	   List<Medico> findByEmail(String email);
+	
+	}
 	
 
-}
+
--- src/main/java/med/voll/api/service/MedicoService.java
+++ src/main/java/med/voll/api/service/MedicoService.java
@@ -3,7 +3,9 @@ package med.voll.api.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
@@ -12,32 +14,62 @@ import med.voll.api.repositories.MedicoRepository;
 
 @Service
 public class MedicoService {
-	
+
 	@Autowired
 	private MedicoRepository repository;
 
-	
 	public Medico save(DadosCadastroMedico dados) {
-		 Medico medico = new Medico(dados);
+
+		Medico medico = new Medico(dados);
+		crmJaExiste(medico);
+		emailJaExiste(medico);
 		return repository.save(medico);
+
 	}
-	
+
+	private boolean crmJaExiste(Medico medico) {
+		var result = repository.findByCrm(medico.getCrm());
+		String crm;
+		for (Medico x : result) {
+			crm = x.getCrm();
+			if (crm != null) {
+				throw new ResponseStatusException(HttpStatus.CONFLICT, "CRM já cadastrado.");
+
+			}
+
+		}
+
+		return true;
+
+	}
+
+	private boolean emailJaExiste(Medico medico) {
+		var result = repository.findByEmail(medico.getEmail());
+		String email;
+		for (Medico x : result) {
+			email = x.getEmail();
+			if (email != null) {
+				throw new ResponseStatusException(HttpStatus.CONFLICT, "Email já cadastrado.");
+
+			}
+
+		}
+
+		return true;
+
+	}
+
 	public Page<DadosListagemMedico> findAllByAtivoTrue(Pageable paginacao) {
-		return repository.
-				findAllByAtivoTrue(paginacao).
-				map(DadosListagemMedico::new);
+		return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
 	}
-	
+
 	public Medico getReferenceById(Long id) {
 		return repository.getReferenceById(id);
-		
+
 	}
 
-	public void delete(Long id) { 
-		 repository.deleteById(id);
-		                                      }
-	
-	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 
-	
 }
