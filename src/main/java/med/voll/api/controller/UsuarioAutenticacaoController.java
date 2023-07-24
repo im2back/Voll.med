8ddb005ddd54d349d875641d7e09aba9558945cd
Jefferson Richards <jefferson.richards@outlook.com>
2023-07-24 11:54:28 -0300

    Adicionado a biblioteca Auth0 e Gerado o Token + retorno padrão DTO, Configurado a senha da API.

--- src/main/java/med/voll/api/controller/UsuarioAutenticacaoController.java
+++ src/main/java/med/voll/api/controller/UsuarioAutenticacaoController.java
@@ -10,23 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import med.voll.api.usuario.DadosAutenticacao;
+import med.voll.api.usuario.Usuario;
 
 @RestController
 @RequestMapping("/login")
 public class UsuarioAutenticacaoController {
 	
-
+	@Autowired
+	private  TokenService tokenService;
 	
 	@Autowired
 	private  AuthenticationManager manager;
 	
+	@SuppressWarnings("rawtypes")
 	@PostMapping
 	public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
 		
-		var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-		var authentication = manager.authenticate(token);
-		return ResponseEntity.ok().build();	
+		var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+		var authentication = manager.authenticate(authenticationToken);
+		var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+		return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));	
 		
 	}
 	
